6ac37fe1eb984d4de05d389c40f2a63705a5c59d
@MarcoGonzalo <marcogonzalo@gmail.com>
2020-12-13 12:30:57 +0100

    Test if TitleContainer reders its content

--- src/components/TitleContainer/__test__/TitleContainer.test.js
+++ src/components/TitleContainer/__test__/TitleContainer.test.js
@@ -9,9 +9,23 @@ configure({ adapter: new Adapter() });
 
 describe('<TitleContainer />', () => {
     beforeEach(() => { sinon.stub(console, 'error'); });
-    afterAll(() => { console.error.restore(); });
-    it('should throw an error if there is no title', () => {
-        const component = shallow(<TitleContainer />);
-        sinon.assert.calledOnceWithMatch(console.error, 'The prop `title` is marked as required');
+    afterEach(() => { console.error.restore(); });
+    describe('props validation', () => {
+        it('should throw an error if there is no title', () => {
+            const component = shallow(<TitleContainer />);
+            sinon.assert.calledOnceWithMatch(console.error, 'The prop `title` is marked as required');
+        });
     });
+    it('should render a title', () => {
+        const component = shallow(<TitleContainer />);
+        component.setProps({ title: "Title test" });
+        expect(component.find('.title-container__title')).toHaveLength(1);
+        expect(component.find('.title-container__title').text()).toBe('Title test');
+    });
+    it('should render a subtitle', () => {
+        const component = shallow(<TitleContainer />);
+        component.setProps({ title: "Title test", subtitle: "Subtitle test" });
+        expect(component.find('.title-container__subtitle')).toHaveLength(1);
+        expect(component.find('.title-container__subtitle').text()).toBe('Subtitle test');
+    })
 });
